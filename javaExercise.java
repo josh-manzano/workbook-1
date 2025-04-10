f1d28ce567d7e776424e2d336de5271c7c9acd3a
Josh M <manzojosh27@gmail.com>
2025-04-10 10:22:48 -0700

    initial java exercise

---
+++ javaExercise.java
@@ -0,0 +1,21 @@
+
+
+public class JavaExercise {
+    public static void main(String[] args) {
+        
+        int whlnum = 56;
+
+        double dblnum = 4.5;
+
+        char ltr = 'A';
+
+        System.out.println(whlnum + dblnum + ltr);
+
+        System.out.println(dblnum);
+
+        System.out.println(ltr);
+
+    }
+
+    
+}
---
+++ workbookexercise.java
@@ -0,0 +1,27 @@
+public class workbookexercise {
+    public static void main(String[] args){
+
+        long vehicleid = 1200000L;
+
+        String vehicleModel = "Toyota Supra";
+
+        String vehicleColor = "Black";
+
+        boolean towingPack = true;
+
+        double odometer = 230;
+
+        double carPrice = 90000.99;
+
+        char qualityRating = 'A';
+
+        String phonenum = "323-374-7387";
+
+        String ssn = "736-74-7489";
+
+        int zip = 90863;
+
+    }
+
+
+}
